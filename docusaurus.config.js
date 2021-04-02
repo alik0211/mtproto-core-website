7b7075d74a6da13314c6c9edbda147beeb148724
Ali Gasymov <alik0211alik@gmail.com>
2021-04-02 17:51:42 +0300

    Add GA

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,11 @@ module.exports = {
   favicon: 'img/favicon.ico',
   organizationName: 'alik0211',
   projectName: 'mtproto-core-website',
+  plugins: ['@docusaurus/plugin-google-analytics'],
   themeConfig: {
+    googleAnalytics: {
+      trackingID: 'UA-112044218-14',
+    },
     prism: {
       theme: require('prism-react-renderer/themes/vsDark'),
     },
